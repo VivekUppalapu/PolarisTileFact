d165ddf85fc1d97d97c48fb90c61d5267205c2cf
VivekUppalapu <rrvivek16@gmail.com>
2023-10-31 18:34:08 -0400

    completed project2

--- src/polaris-tile-fact.js
+++ src/polaris-tile-fact.js
@@ -5,16 +5,17 @@ import { LitElement, html, css } from 'lit';
 class PolarisTileFact extends LitElement {
   static get properties() {   
     return {
+
       outerLayer:{},
-      outerLayer1:{},
-      type1Fact:{},
-      textFact1:{type: String},
-      type2Fact:{},
-      textFact2:{type: String},
-      type3Fact: {},
-      textFact3:{type: String},
-      type4Fact:{},
-      textFact4:{type: String},
+      outerLayerML:{},
+      tileChange:{type: String, reflect: true},
+      textChange:{type: String, reflect: true},
+      textFact: {type:String},
+      multiline: {type: Boolean},
+      hasLink:{type: Boolean},
+      paraText:{},
+      borderColor: {},
+      link: {type: String},
     };
   }
 
@@ -23,80 +24,150 @@ class PolarisTileFact extends LitElement {
 
       :host {
         display: block;
-        margin-left: 40px;
-        margin-right: 40px;
-        //background-color: #8ac3f3;
+        margin-left: 5px;
+        margin-right: 5px;
       }
 
       .outerLayer
       {
-        display: flex;
+        display: grid;
+        height: 36vw;
+        padding: 0 14% 0 14%;
         margin-top: 32px;
+        
       }
-      .outerLayer1
+      .outerLayerML
       {
-        display: flex;
-        margin-top: 32px;
+        display: grid;
+        padding: 0 14% 0 14%;
+
       }
-      .type1Fact{ 
+      :host([tileChange=type1]) 
+      {
         width: 336px;
         height: 280.8px;
         background-color: #1e407c;
+
       }
-      .textFact1{
-        padding: 0 14% 0 14%;
-        text-align: center;
+     :host([textChange=text1])
+      {
+        font-size: 1.75rem;
+        font-weight: 700px;
+        margin-bottom: 1rem;
         font-family: Roboto,"Franklin Gothic Medium",Tahoma,sans-serif;
-        font-size: 32px;
-        color: white;
+        color: white; 
       }
-      .type2Fact{
+      :host([tileChange=type2])
+      {
         width: 336px;
         height: 280.8px;
-        background-color: white;
+        background-color:white;
+        
       }
-      .type3Fact{
+      :host([textChange=text2])
+      {
+        font-family: Roboto,"Franklin Gothic Medium",Tahoma,sans-serif;
+        font-size: 1.75rem;
+        color:  #001e44;
+        font-weight: 900 px;
+      }
+      :host([tileChange=type3])
+      {
         width: 336px;
         height: 280.8px;
         background-color: #001e44;
       }
-      .type4Fact{
+      :host([tileChange=type4])
+      {
         width: 336px;
         height: 280.8px;
-        //background-color: #001e44;
         background: linear-gradient(180deg, rgba(30,64,124,1) 0%, rgba(0,30,68,1) 65%, rgba(0,30,68,1) 100%);
       }
-      .textFact4{
-        padding: 0 14% 0 14%;
-        text-align: center;
-        font-family: Roboto,"Franklin Gothic Medium",Tahoma,sans-serif;
-        font-size: 32px;
-        color: white;
+
+      :host([tileChange=type5])
+      {
+        width: 336px;
+        height: 280.8px;
+        background-color: rgba(0, 3, 33, 0.5);
+        background-image: url(../assets/penn-state-tuition.jpg);
+        background-size: cover;
+        background-position: center;
+        background-blend-mode: multiply;
       }
+      
+      :host([tileChange=type6])
+      {
+        width: 336px;
+        height: 280.8px;
+        background-color: rgba(0, 3, 33, 0.5);
+        background-image: url(../assets/data-digest.jpg);
+        background-size: cover;
+        background-position: center;
+        background-blend-mode: multiply;
+      }
+      .paraText{
+        font-size: 1rem;
+        font-weight: 400;
+        color: #001e44;
+        font-family: Roboto,"Franklin Gothic Medium",Tahoma,sans-serif;
+      }
+      .borderColor{
+      margin-top: 1.35rem;
+      margin-bottom: 1rem;
+      background-color: #1e407c;
+      height: 3px;
+      }
+      .link
+      {
+        font-family: Roboto,"Franklin Gothic Medium",Tahoma,sans-serif;
+        color: white; 
+        text-decoration: none;
+      }
+      
+
     `;
   }
 
   constructor() {
     super();
-    this.textFact1 = '';
-    this.textFact2 = '';
-    this.textFact3 = '';
-    this.textFact4 = '';
+    this.textFact = '';
+    this.tileChange = ''
+    this.textChange = '';
+    this.multiline = false;
+    this.hasLink = false;
+    this.link='';
+    this.paraText;
 
   }
 
   render() {
-    return html`
-    <div class="outerLayer">
-    <div class="type1Fact"><h3 class="textFact1"><slot>${this.textFact1}</slot></h3></div>
-    <div class="type2Fact"><h3 class="textFact2"><slot>${this.textFact2}</slot></h3></div>
-    <div class = "type4Fact"><h3 class = "textFact4"><slot>${this.textFact4}</slot></h3></div>
-    </div>
-    <div class="outerLayer">
-    <div class="type1Fact"><h3 class="textFact1"><slot>${this.textFact1}</slot></h3></div>
-    <div class="type3Fact"><h3 class="textFact3"><slot>${this.textFact3}</slot></h3></div>
-    </div>
+    if(this.multiline == true){
+      return html`
+      <div class="outerLayerML">
+      <div class=${this.tileChange}><h3 class=${this.textChange}><slot>${this.textFact}</slot></h3></div>
+      <b class="borderColor"> </b>
+      <p class="paraText"> ${this.paraText} </p>
+      </div>
+      `;
+    }
+    if(this.hasLink == true)
+    {
+      return html`
+      <div class="outerLayer">
+      <a class="link" href="${this.link}">
+      <div class=${this.tileChange}><h3 class=${this.textChange}><slot>${this.textFact}</slot></h3></div>
+     </a>
+      </div>
+      `
+    }
+    else
+    { 
+        return html`
+        <div class="outerLayer">
+      <div class=${this.tileChange}><h3 class=${this.textChange}><slot>${this.textFact}</slot></h3></div>
+      </div>
     ` ;
+    }  
   }
 }
 
